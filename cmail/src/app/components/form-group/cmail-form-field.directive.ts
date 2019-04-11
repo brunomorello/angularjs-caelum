521ada1bcb03d497902c2954430f0c5b97d00c7f
Bruno Moreno <brunomorello7@gmail.com>
2019-04-10 22:51:57 -0300

    Created a Directive for FormGroups

---
+++ cmail/src/app/components/form-group/cmail-form-field.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[cmailFormField]'
+})
+export class CmailFormFieldDirective {
+
+  // DI - Dependency Inject * Bring Classes or Types that Angular knows how to create
+  //element.nativeElement instantiate the DOM Attributes to be handled
+  //     console.log(element.nativeElement); 
+  constructor(element: ElementRef) { }
+
+  ngOnInit() {
+
+  }
+
+  //  id="user-full-name" placeholder=" " class="mdl-textfield__input"
+
+}
