6f38cc122209b6206dfa7f9aa0819d22403401aa
Bruno Moreno <brunomorello7@gmail.com>
2019-04-11 19:45:37 -0300

    Exploring FormControl and its properties

--- cmail/src/app/models/register/register.component.ts
+++ cmail/src/app/models/register/register.component.ts
@@ -9,15 +9,30 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   registerForm = new FormGroup({
-    'user-full-name': new FormControl(),
-    'user-username': new FormControl(),
-    'user-password': new FormControl(),
-    'user-avatar': new FormControl()
+    'user_full_name': new FormControl(),
+    'user_username': new FormControl(),
+    'user_password': new FormControl(),
+    'user_avatar': new FormControl()
   });
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleRegisterUser() {
+
+    let formValues = this.registerForm.value;
+
+    console.log(`formValid? ${formValues.status}`);
+    console.log(formValues); 
+
+    console.log(`user_full_name ${formValues.user_full_name}`);
+    console.log(`user_username ${formValues.user_username}`);
+    console.log(`user_password ${formValues.user_password}`);
+    console.log(`user_avatar ${formValues.user_avatar}`);
+    console.log(this.registerForm);
+    
   }
 
 }
