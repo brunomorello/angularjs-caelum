df89bff9e54c989114aeee57f1303e2335a1c0c5
Bruno Moreno <brunomorello7@gmail.com>
2019-04-12 22:47:41 -0300

    Created shared-components for this application

---
+++ cmail/src/app/components/shared-components/shared-components.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cmail-shared-components',
+  templateUrl: './shared-components.component.html',
+  styles: []
+})
+export class SharedComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
