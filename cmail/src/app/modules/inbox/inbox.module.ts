225b35ece95539134e9ceca2fb15d1d005cb8835
Bruno Moreno <brunomorello7@gmail.com>
2019-04-15 22:35:26 -0300

    Removed unused Modules

--- cmail/src/app/modules/inbox/inbox.module.ts
+++ cmail/src/app/modules/inbox/inbox.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InboxComponent } from "./inbox.component";
+import { InboxRoutingModule } from './inbox-routing.module';
+import { SharedComponentsModule } from 'src/app/components/shared-components.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [InboxComponent],
+  declarations: [
+    InboxComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedComponentsModule,
+    InboxRoutingModule
   ]
 })
 export class InboxModule { }
