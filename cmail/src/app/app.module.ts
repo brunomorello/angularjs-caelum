f8f34320f1c60cca719c3db9b9b0382b20806d67
Bruno Moreno <brunomorello7@gmail.com>
2019-04-15 22:04:02 -0300

    Removed HTTPClientModule, HelperModule and SharedComponents

--- cmail/src/app/app.module.ts
+++ cmail/src/app/app.module.ts
@@ -2,30 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { LoginComponent } from './modules/login/login.component';
-import { RegisterComponent } from './modules/register/register.component';
-import { InboxComponent } from './modules/inbox/inbox.component';
-import { RoutingModule } from "./app.routes";
-import { FormGroupComponent } from './components/form-group/form-group.component';
-import { CmailFormFieldDirective } from './components/form-group/cmail-form-field.directive';
-import { HttpClientModule } from '@angular/common/http';
-import { SharedComponentsComponent } from './components/shared-components/shared-components.component';
+import { CustomRouterModule } from "./app.routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent,    
-    RegisterComponent,
-    InboxComponent,    
-    SharedComponentsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RoutingModule,
-    HttpClientModule
+    CustomRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
