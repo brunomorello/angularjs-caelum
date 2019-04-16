4d233880ef593e762a654f7c1102e8d147ceec9f
Bruno Moreno <brunomorello7@gmail.com>
2019-04-15 22:36:18 -0300

    Login View and Authenticate implemented

--- cmail/src/app/modules/login/login.component.ts
+++ cmail/src/app/modules/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'cmail-login',
@@ -9,13 +11,49 @@ import { ActivatedRoute } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   userId = '';
+  password = '';
 
-  constructor(private activeRoute: ActivatedRoute) { }
+  constructor(private activeRoute: ActivatedRoute, private httpClient: HttpClient) { }
 
   ngOnInit() {
     console.log(this.activeRoute.snapshot.paramMap);
 
     this.userId = this.activeRoute.snapshot.paramMap.get('id');
+    
+  }
+
+  handleLogin(loginForm: NgForm) {
+
+    if(!loginForm.valid) {
+
+      console.log(loginForm);
+
+      loginForm.control.get('userId').markAsTouched();
+      loginForm.control.get('password').markAsTouched();
+      return;
+    }
+    
+    let data = {
+      email: loginForm.value.userId,
+      password: "123"
+    }
+
+    this.httpClient
+      .post('http://localhost:3200/login', data)
+      .subscribe(
+        (response: any) => {
+          
+          console.log(response);
+          localStorage.setItem("cmail-token", response.token)
+          this.loginRoutes.navigate(['/inbox']);
+          
+        },
+        (httpError: HttpErrorResponse) => {
+          console.log(httpError);
+        }
+      )
+
+    //console.log(loginForm);
     
   }
 
--- cmail/src/app/modules/login/login.module.ts
+++ cmail/src/app/modules/login/login.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SharedComponentsModule } from 'src/app/components/shared-components.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedComponentsModule,
+    LoginRoutingModule,
+    HttpClientModule
   ]
 })
 export class LoginModule { }
