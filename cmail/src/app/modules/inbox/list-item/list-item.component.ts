c8030f8eb3380bd00560ace0bb6065629d4ae1d6
Bruno Moreno <brunomorello7@gmail.com>
2019-04-16 22:48:38 -0300

    Created Component to list emails on inbox: ng g c list-item

---
+++ cmail/src/app/modules/inbox/list-item/list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cmail-list-item',
+  templateUrl: './list-item.component.html',
+  styles: []
+})
+export class ListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
