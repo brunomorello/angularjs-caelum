18e29b0d32c46c6d0a8a5235264628e27229492f
Bruno Moreno <brunomorello7@gmail.com>
2019-04-16 23:10:08 -0300

    Implementing Input for EmailList

--- cmail/src/app/modules/inbox/list-item/list-item.component.ts
+++ cmail/src/app/modules/inbox/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'cmail-list-item',
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ListItemComponent implements OnInit {
+
+  @Input() emailTo = '';
+  @Input() emailSubject = '';
+  @Input() emailContent = '';
+  @Input() emailCreationDate = '';
 
   constructor() { }
 
