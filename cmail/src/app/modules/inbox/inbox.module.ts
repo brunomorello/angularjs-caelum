b2966241e22c279f0f68ea65c45e66a06e4e77f4
Bruno Moreno <brunomorello7@gmail.com>
2019-04-16 22:54:12 -0300

    Imported HttpClientModule

--- cmail/src/app/modules/inbox/inbox.module.ts
+++ cmail/src/app/modules/inbox/inbox.module.ts
@@ -4,17 +4,25 @@ import { InboxComponent } from "./inbox.component";
 import { InboxRoutingModule } from './inbox-routing.module';
 import { SharedComponentsModule } from 'src/app/components/shared-components.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EmailService } from 'src/app/services/email.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    InboxComponent
+    InboxComponent,
+    ListItemComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
     SharedComponentsModule,
-    InboxRoutingModule
+    InboxRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    EmailService
   ]
 })
 export class InboxModule { }
