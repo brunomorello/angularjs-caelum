63ad398b8dd93f86a6117f6ed7fc9ee757e0e212
Bruno Moreno <brunomorello7@gmail.com>
2019-04-16 21:31:13 -0300

    Implemented AuthGuard

---
+++ cmail/src/app/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivate, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private router: Router) {
+
+    }
+
+    canActivate(): boolean {
+        
+        if(localStorage.getItem('cmail-token')) {
+            return true;
+        } else {
+            this.router.navigate([''])                
+            return false;
+        }
+    }
+
+}
