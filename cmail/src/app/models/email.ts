27ef3d00c0665c46fd783981593f42b22ff6223e
Bruno Moreno <brunomorello7@gmail.com>
2019-04-17 20:34:41 -0300

    Implemented a constructor

--- cmail/src/app/models/email.ts
+++ cmail/src/app/models/email.ts
@@ -3,6 +3,15 @@ export class Email {
     subject = '';
     content = '';
     creationDate: Date;
+    
+    constructor(
+        {to, subject, content, creationDate}:
+        {to: string, subject: string, content: string, creationDate: Date}) {
+        this.to = to;
+        this.subject = subject;
+        this.content = content;
+        this.creationDate = creationDate;
+    }
 
     get contentIntroduction() {
         return this.content.substr(0, 40) + '...';
