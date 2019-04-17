fc8fce2a610ff9d14ea6f9020d70b2c2c47a47a0
Bruno Moreno <brunomorello7@gmail.com>
2019-04-17 19:50:24 -0300

    Inserted a type Date for creationDate

--- cmail/src/app/models/email.ts
+++ cmail/src/app/models/email.ts
@@ -2,7 +2,7 @@ export class Email {
     to = '';
     subject = '';
     content = '';
-    creationDate = '';
+    creationDate: Date;
 
     get contentIntroduction() {
         return this.content.substr(0, 40) + '...';
