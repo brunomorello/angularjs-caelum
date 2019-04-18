00bf1edf7234310562c9ea37f9dcc792436832ce
Bruno Moreno <brunomorello7@gmail.com>
2019-04-17 22:20:47 -0300

    ID implemented

--- cmail/src/app/models/email.ts
+++ cmail/src/app/models/email.ts
@@ -3,15 +3,17 @@ export class Email {
     subject = '';
     content = '';
     creationDate: Date;
+    id = '';
     
     constructor(
-        {to, subject, content, creationDate}:
-        {to: string, subject: string, content: string, creationDate: Date}
+        {to, subject, content, creationDate, id}:
+        {to: string, subject: string, content: string, creationDate: Date, id: string}
     ) {
         this.to = to;
         this.subject = subject;
         this.content = content;
         this.creationDate = creationDate;
+        this.id = id;
     }
-    
+
 }
